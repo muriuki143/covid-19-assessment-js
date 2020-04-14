195183076f2876701dde59adf5ed31f602b96e11
muriuki143 <53922082+muriuki143@users.noreply.github.com>
2020-04-14 14:27:28 +0300

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
 const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
+"data"{
+	"region":{
+		"name": "Kenya",
+		"avgDailyIncomeinUSD": 3,
+		"avgDailyIncomePopulation": 0.85,
+		"periodType":"days", 
+		"timeToElapse": 30,
+		"reportedCases": 208,
+		"population": 47108327,
+		"totalHospitalBeds": 8192,
+	}
+},
+"impact":{
+		"currentlyInfected": 2080,
+		"infectedByRequestedTime": 2129920,
+		"severeCasesByRequestedTime": 319488,
+		"hospiitalBedsByRequestedTime": -316620,
+		"casesForICURequestedTime": 106496,
+		"casesForVentlitorsByRequestedTime" 6389,
+		"dollarsInFlight" 108,625,920
+},
+"severeImpact"{
+		"currentlyInfected": 10400,
+		"infectedByRequestedTime": 10649600,
+		"severeCasesByRequestedTime": 1597440,
+		"hospiitalBedsByRequestedTime": -1594572,
+		"casesForICURequestedTime": 532480,
+		"casesForVentlitorsByRequestedTime" 212992,
+		"dollarsInFlight" 543129600,
+}
